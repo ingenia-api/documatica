f1260a3a84bbf3b9634a45a40e17bc15b32ea670
Joran Kikke <joran.k@gmail.com>
2014-02-12 12:05:41 +0000

    working thor generator

--- lib/doc_smoosher.rb
+++ lib/doc_smoosher.rb
@@ -10,18 +10,29 @@ module DocSmoosher
 
 
   module Generators
-    class Recipe < Thor::Group
+    class Api < Thor::Group
       include Thor::Actions
-
       argument :name, :type => :string
+      
+      class_option :test_framework, :default => :test_unit
+
+      def self.source_root
+        File.dirname(__FILE__)
+      end
+
+      def create_api_dir
+
+        puts "creating app dir"
+        create_file name
+      end
     end
   end
 
 
-  def self.help
-    puts 'Useage: '
-    puts '    smoosher generate NewApiName'
-    puts '      - Builds a new scaffold template called NewApiName for your API'
-  end
+  # def self.help
+  #   puts 'Useage: '
+  #   puts '    smoosher generate NewApiName'
+  #   puts '      - Builds a new scaffold template called NewApiName for your API'
+  # end
 
 end
